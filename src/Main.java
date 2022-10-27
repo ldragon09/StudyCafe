6749bee12f3822895de8675c5a4a529d6fca9e46
ldragon09 <ldragon09@naver.com>
2022-10-27 23:37:12 +0900

    회원가입 기능 구현 후 마이 페이지 기능 구현중

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +7,7 @@ public class Main {
         System.out.println("== 프로그램 시작 ==");
         System.out.println("안녕하세요 스터디카페입니다!");
         Scanner sc = new Scanner(System.in);
-
+        List<Member> members = new ArrayList<>();
         while (true){
             System.out.println("명령어) ");
             String cmd = sc.nextLine();
@@ -20,9 +22,12 @@ public class Main {
                 System.out.println("회원가입이 완료되었습니다.");
 
                 Member member = new Member(id, pw, name);
+                members.add(member);
+                System.out.printf("%s회원님 환영합니다\n", name);
 
-                System.out.printf("%s회원님 환영합니다\n", id);
-
+            }if(cmd.equals("mypage")){
+                Member members1 = new Member();
+                System.out.printf("현재 로그인 되어있는 아이디는 %s입니다.", members1.id);
             }else if(cmd.equals("ruls")){
                 System.out.println("규칙\n1. 소란스럽지 않게 하기\n2. 퇴실시 뒷정리 깔끔히하기\n3. 입, 퇴실 등록 잘하기");
             }else if(cmd.equals("price")){
@@ -44,10 +49,13 @@ class Member{
     String id;
     String pw;
     String name;
-    Member(String id, String pw, String name){
+    public Member(String id, String pw, String name){
         this.id = id;
         this.pw = pw;
         this.name = name;
     }
 
+    public Member() {
+
+    }
 }
