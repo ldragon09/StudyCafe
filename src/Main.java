da188915a6bfffaaf791f0cac1b82ed053544bf1
ldragon09 <ldragon09@naver.com>
2022-11-15 19:19:17 +0900

    로그인 구현 중, 회원가입 시간기능 추가 중.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static Object getNowDateStr;
+
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         System.out.println("안녕하세요 스터디카페입니다!");
         Scanner sc = new Scanner(System.in);
         List<Member> members = new ArrayList<>();
         while (true){
+            String regDate = Util.getNowDateStr();
             System.out.println("명령어) ");
             String cmd = sc.nextLine();
             if(cmd.equals("memberNew")){
@@ -19,6 +22,7 @@ public class Main {
                 String pw = sc.nextLine();
                 System.out.println("NAME : ");
                 String name = sc.nextLine();
+                System.out.println(getNowDateStr);
                 System.out.println("회원가입이 완료되었습니다.");
 
 
@@ -34,13 +38,13 @@ public class Main {
                 String loginId = sc.nextLine();
                 System.out.println("PW: ");
                 String loginPw = sc.nextLine();
-
+                System.out.println("로그인에 성공하였습니다!");
 
 
 
             }if(cmd.equals("mypage")){
                 Member members1 = members.get(0);
-                System.out.printf("현재 로그인 되어있는 아이디는 %s입니다.\n", members1.id);
+                System.out.printf("현재 로그인 되어있는 아이디는 %s입니다. %s부터 가입되어있습니다.\n", members1.id, getNowDateStr);
             }else if(cmd.equals("ruls")){
                 System.out.println("규칙\n1. 소란스럽지 않게 하기\n2. 퇴실시 뒷정리 깔끔히하기\n3. 입, 퇴실 등록 잘하기");
             }else if(cmd.equals("price")){
@@ -58,7 +62,9 @@ public class Main {
         }
         System.out.println("== 프로그램 종료 ==");
     }
-}
+
+    }
+
 class Member{
     String id;
     String pw;
